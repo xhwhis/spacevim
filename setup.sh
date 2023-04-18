d879af4ca46961a8b7f2795e8b729de0c2640b25
xhwhis <hi@whis.me>
2023-04-18 18:19:44 +0800

    feat: setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+if which brew >/dev/null; then
+    brew install neovim node
+fi
+
+curl -sLf https://spacevim.org/install.sh | bash
+
+echo "Trying to clone SpaceVim.d"
+rm -rf .SpaceVim.d && git clone https://github.com/xhwhis/spacevim.git .SpaceVim.d
+nvim -c "SPInstall" -c "q" -c "CocInstall" -c "q" -c "q"
+
+echo "Installation complete"
